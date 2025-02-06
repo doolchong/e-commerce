667cc54523863e4d2d27f107baf8e3ca9a3373d4
Your Name <doolchong@gmail.com>
2025-02-06 21:11:01 +0900

    User Service 서버 상태 체크 api 구현

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping("health-check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+}
